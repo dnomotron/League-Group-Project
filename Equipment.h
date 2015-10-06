d905811aa17857217e0f94859bcdd59ebc1d4940
strap8 <nateinthegame@gmail.com>
2015-10-06 11:43:15 -0700

    Created Equipment class

--- Equipment.h
+++ Equipment.h
@@ -1,1 +1,38 @@
 
+//  Dalton Rick, Nathan Foster, Kush Patel, Desmond Lee, Kurtis Hoang
+//
+//  CIS 22C
+//
+//  Group Project - Leaugue of Legends Character engine
+//
+//  Hero.h
+
+
+#ifndef ____Equipment__
+#define ________Equipment____
+
+class Equipment{
+
+private:
+
+public:
+
+	//Default Constructor
+	
+
+	//Copy Constructor
+
+	//Default Constuctor
+	
+	// ** Setters (manipulators) **
+	
+
+	// ** Accessors **
+
+
+	// ** Other Member Functions **
+	
+
+};
+
+#endif /* defined(____Equipment__) */
